587de9856c75dac55b0ffea52a8a16b632433af1
Ela Jahoda <3la.jah0da@gmail.com>
2022-04-23 15:21:02 +0200

    generating elements for class Recepty

--- app.js
+++ app.js
@@ -17,3 +17,31 @@ recept-hodnoceni, recept-nazev, recept-popis.
 
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
+var kontejner = document.querySelector('.kontejner');
+
+generovaniReceptu();
+
+function generovaniReceptu() {
+    for(i = 0; i < recepty.length; i++) {
+        let recept = document.createElement('div');
+        recept.className = 'recept';
+
+        let receptObrazek = document.createElement('div');
+        receptObrazek.className = 'recept-obrazek';
+
+        let obrazek = document.createElement('img');
+        obrazek.src = recepty[i].img;
+
+        let receptInfo = document.createElement('div');
+        receptInfo.className = 'recept-info';
+
+        let nazev = document.createElement('h3');
+        nazev.textContent = recepty[i].nadpis;
+
+        kontejner.appendChild(recept);
+        recept.appendChild(receptObrazek);
+        receptObrazek.appendChild(obrazek);
+        recept.appendChild(receptInfo);
+        receptInfo.appendChild(nazev);
+    }
+}
