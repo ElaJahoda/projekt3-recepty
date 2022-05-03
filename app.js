b5f0bb2b190d255c2cb8d08316ea90cde3248c20
Ela Jahoda <3la.jah0da@gmail.com>
2022-05-03 10:52:23 +0200

    refactoring

--- app.js
+++ app.js
@@ -17,46 +17,41 @@ recept-hodnoceni, recept-nazev, recept-popis.
 
 6) Poslední vybraný recept ulož do Local Storage, aby se při novém otevření aplikace načetl.
 */
-var kontejner = document.querySelector('.kontejner');
-var tlacitko = document.querySelector('button');
-var kategorie = document.querySelector('#kategorie');
-var razeni = document.querySelector('#razeni');
-generovaniSeznamu();
+let kontejner = document.querySelector('.kontejner');
+let tlacitko = document.querySelector('button');
+let hledat = document.querySelector('#hledat');
+let kategorie = document.querySelector('#kategorie');
+let razeni = document.querySelector('#razeni');
+let receptyId = document.querySelector('.recepty');
 
-function generovaniSeznamu() {
-    generovaniReceptyId();
-    for(i = 0; i < recepty.length; i++) {
-        generovaniPolozky(i);
-    }
+let arrFiltrace = [];
+
+generovaniSeznamu(recepty);
+
+function generovaniSeznamu(pole) {
+    smazaniSeznamu();
+    for(i = 0; i < pole.length; i++) {
+    generovaniPolozky(pole, i);
+    }  
     if(localStorage.indexPolozky !== "") {
         let item = localStorage.getItem('indexPolozky');
         detailPolozky(item);}
 }
 
-function generovaniReceptyId() {
-        let receptyId = document.createElement('div');
-        receptyId.className = 'recepty';
-        receptyId.setAttribute('id', 'recepty');
-        kontejner.appendChild(receptyId);
-}
-
-function generovaniPolozky(i) {
+function generovaniPolozky(pole, i) {
     let recept = document.createElement('div');
     recept.className = 'recept';
-    recept.dataset.index = i;
     let receptObrazek = document.createElement('div');
     receptObrazek.className = 'recept-obrazek';
 
     let obrazek = document.createElement('img');
-    obrazek.src = recepty[i].img;
+    obrazek.src = pole[i].img;
 
     let receptInfo = document.createElement('div');
     receptInfo.className = 'recept-info';
 
     let nazev = document.createElement('h3');
-    nazev.textContent = recepty[i].nadpis;
-
-    let receptyId = document.querySelector('.recepty');
+    nazev.textContent = pole[i].nadpis;
 
     receptyId.appendChild(recept);
     recept.appendChild(receptObrazek);
@@ -64,90 +59,48 @@ function generovaniPolozky(i) {
     recept.appendChild(receptInfo);
     receptInfo.appendChild(nazev);
 
-    recept.addEventListener('click', () => detailPolozky(i))
+    recept.addEventListener('click', () => detailPolozky(i, pole))
 }
 
-function detailPolozky(i) {
-    document.querySelector('#recept-foto').src = recepty[i].img;
-    document.querySelector('#recept-kategorie').textContent = recepty[i].kategorie;
-    document.querySelector('#recept-hodnoceni').textContent = recepty[i].hodnoceni;
-    document.querySelector('#recept-nazev').textContent = recepty[i].nadpis;
-    document.querySelector('#recept-popis').textContent = recepty[i].popis;     
+function detailPolozky(i, pole) {
+    console.log(i);
+    console.log(pole);
+    document.querySelector('#recept-foto').src = pole[i].img;
+    document.querySelector('#recept-kategorie').textContent = pole[i].kategorie;
+    document.querySelector('#recept-hodnoceni').textContent = pole[i].hodnoceni;
+    document.querySelector('#recept-nazev').textContent = pole[i].nadpis;
+    document.querySelector('#recept-popis').textContent = pole[i].popis;     
     localStorage.clear();
     localStorage.setItem('indexPolozky', i);
 }
 
-tlacitko.addEventListener('click', filtrace);
-
-var poleNazvy = [];
-   for(i = 0; i < recepty.length; i++) {
-        poleNazvy.push(recepty[i].nadpis);
-    }
-var poleKategorie = [];
-    for(i = 0; i < recepty.length; i++) {
-        poleKategorie.push(recepty[i].kategorie);
-    }
+hledat.addEventListener('change', filtrace);
+kategorie.addEventListener('change', filtrace);
+razeni.addEventListener('change', filtrace);
 
 function filtrace() {
-    smazaniSeznamu();
-    let hodnotaText = document.querySelector('#hledat').value;
-    let hodnotaKategorie = kategorie.value; 
-    let hodnotaRazeni = razeni.value;
-    if(hodnotaText === "" && hodnotaKategorie === "" && hodnotaRazeni === "") {
-        generovaniSeznamu();
-    }
-    else if(hodnotaRazeni !== "" && hodnotaText === "" && hodnotaKategorie === "") {        
-        if(hodnotaRazeni == 2) {
-            recepty.sort(function (a, b) {
-            return a.hodnoceni - b.hodnoceni;
-        })} 
-        if(hodnotaRazeni == 1) {
-            recepty.sort(function (a, b) {
-            return b.hodnoceni - a.hodnoceni;  
-        })}
-        generovaniSeznamu();
-    }
-    else if(hodnotaText !== "" && hodnotaRazeni === "" && hodnotaKategorie === "") {
-        let filtrNazvyPole = filterText(poleNazvy, hodnotaText);
-        generovaniReceptyId();
-        for(i = 0; i < filtrNazvyPole.length; i++) {
-            let index = poleNazvy.indexOf(filtrNazvyPole[i]);
-            generovaniPolozky(index);
-        }
-    }
-    else if (hodnotaKategorie !== "" && hodnotaRazeni === "" && hodnotaText === "") {
-        let filtrKategoriePoleI = [];
-        filtrKategorie(poleKategorie, hodnotaKategorie, filtrKategoriePoleI);
-        generovaniReceptyId();
-        for(i = 0; i < filtrKategoriePoleI.length; i++) {
-            generovaniPolozky(filtrKategoriePoleI[i]);
-        }
-    }
-    else {
-        hodnotaText = "";
-        hodnotaKategorie = "";
-        hodnotaRazeni = "";
-        alert('Lze filtrovat pouze dle jednoho parametru. Obnov stránku a opakuj hledaní.');
-        generovaniSeznamu();
-    }
-}
+    arrFiltrace = recepty.filter((recept) => 
+    recept.nadpis.toLowerCase().includes(hledat.value)
+    );
 
-function filtrKategorie(pole, hodnota, arry) {
-    for ( i=0; i < pole.length; i++ ){
-        if ( pole[i] == hodnota){
-            arry.push(i)
-        }
+    if(kategorie.value !== "") {        
+        arrFiltrace = arrFiltrace.filter(
+            (recept) => recept.kategorie === kategorie.value);
     }
-    return arry;
-} 
 
-function filterText(pole, hodnota) {   
-    return pole.filter(function(el) {
-         return el.toLowerCase().indexOf(hodnota.toLowerCase()) !== -1
-    })
+    if(razeni.value == 2) {
+        arrFiltrace = arrFiltrace.sort(function (a, b) {
+        return a.hodnoceni - b.hodnoceni;
+    })} 
+    if(razeni.value == 1) {
+        arrFiltrace = arrFiltrace.sort(function (a, b) {
+        return b.hodnoceni - a.hodnoceni;  
+    })}
+    console.log(arrFiltrace);
+    generovaniSeznamu(arrFiltrace);
 }
 
 function smazaniSeznamu() {
-    let receptyId = document.querySelector('.recepty');
-    kontejner.removeChild(receptyId);
+let recept = document.querySelectorAll('.recept');
+recept.forEach(item => item.remove());
 }
